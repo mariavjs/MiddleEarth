3ccd7dc80cec83663a8dbc350be053d2fdb7a56a
Camila Consentino <cami.consentino@gmail.com>
2025-10-23 07:55:53 -0300

    commit inicial

---
+++ Assets/MainActions.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1); //cena jogo
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0); //cena menu inicial
+    }
+}
