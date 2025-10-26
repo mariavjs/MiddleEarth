9caf488673e9b9e27aaaf0a9382a8f3e60180004
beatrizbzack <beatrizbz@al.insper.edu.br>
2025-10-26 18:12:46 -0300

    add: player jump animation

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,10 +4,17 @@ public class Player : MonoBehaviour
 {
     public float speed = 5f;
     public float acceleration = 1.2f;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public float jumpHeight = 10f; 
+    private bool canJump = true; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -16,5 +23,32 @@ public class Player : MonoBehaviour
         speed += acceleration * Time.deltaTime;
         // move o player em direção X positiva
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            // pula o player
+            Jump();
+            animator.SetBool("Jump", true);
+            canJump = false; // impede pulo duplo
+        }
+    }
+
+    void Jump()
+    {
+        Vector2 velocity = rb.linearVelocity;
+        velocity.y = jumpHeight; // define a velocidade vertical para o pulo
+        rb.linearVelocity = velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) // if collision.collider.tag == "Ground"
+        {
+            // O player pode pular novamente
+            canJump = true;
+            animator.SetBool("Jump", false);
+
+            // Debug.Log("Player landed on the ground.");
+        }
     }
 }
